2331543928b76c1df9adc2c7ee73ad43b0ea5590
Brett Mandler <brettmandler@gmail.com>
2022-01-04 12:12:10 -0500

    Auto generate supabase types

--- src/util/supabase.ts
+++ src/util/supabase.ts
@@ -1,14 +1,15 @@
 import {createClient, PostgrestResponse} from '@supabase/supabase-js'
+import type {SupabaseQueryBuilder} from '@supabase/supabase-js/dist/main/lib/SupabaseQueryBuilder'
+import type {definitions} from '../generated/supabase'
 
 export const client = createClient(
   'https://lyktewmeyuuswcdvpzwe.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTI4MzMwMywiZXhwIjoxOTU2ODU5MzAzfQ.U7Eb_C1u2flHL1-s0AQGYcGIpA1e03nfiYf--t3nsUk'
 )
 
-interface Email {
-  email: string
-}
+type From = <T extends keyof definitions>(
+  table: T
+) => SupabaseQueryBuilder<definitions[T]>
+export const from: From = client.from
 
-type AddEmail = (email: string) => Promise<PostgrestResponse<Email>>
-export const addEmail: AddEmail = async email =>
-  client.from<Email>('emails').upsert({email})
+export const addEmail = async (email: string) => from('emails').upsert({email})
