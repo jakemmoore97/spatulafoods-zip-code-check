e077abf5cbdd84f06039867d9f7b9b1bfa84f81c
Brett Mandler <brettmandler@gmail.com>
2022-01-04 18:01:36 -0500

    Cleanup

--- src/lib/InputGroup.svelte
+++ src/lib/InputGroup.svelte
@@ -1,3 +1,6 @@
-<div class="flex flex-wrap w-full gap-2 children:(w-full md:w-[unset])">
+<div
+  class="flex flex-wrap w-full gap-2 children:(w-full md:w-[unset])"
+  role="group"
+>
   <slot />
 </div>
