15c147021b59f366325b6822cbd0af15e5720842
Brett Mandler <brettmandler@gmail.com>
2022-01-04 11:18:34 -0500

    Cleanup

--- src/hooks/transformInputValue.ts
+++ src/hooks/transformInputValue.ts
@@ -1,12 +1,11 @@
 type TransformFunction = (str: string) => string
 type ReturnFunction = (node: HTMLInputElement) => void
 
-export const transformInputValue =
-  (transform: TransformFunction): ReturnFunction =>
-  node => {
-    const update = () => (node.value = transform(node.value))
+type TransformInputValue = (transform: TransformFunction) => ReturnFunction
+export const transformInputValue: TransformInputValue = transform => node => {
+  const update = () => (node.value = transform(node.value))
 
-    node.addEventListener('input', update, {capture: true})
+  node.addEventListener('input', update, {capture: true})
 
-    update()
-  }
+  update()
+}
