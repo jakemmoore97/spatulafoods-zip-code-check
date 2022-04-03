6d4cf116699be45bc6e6ec39fddf9ef2e9466bb9
Brett Mandler <brettmandler@gmail.com>
2022-04-03 12:54:16 -0400

    Fix zip store to only use first three characters

--- src/stores/zip.ts
+++ src/stores/zip.ts
@@ -1,4 +1,4 @@
 import {writable} from 'svelte/store'
 const stored = localStorage.zip
 export const zip = writable(stored || '')
-zip.subscribe(value => (localStorage.zip = value))
+zip.subscribe(value => (localStorage.zip = value.slice(0, 3)))
