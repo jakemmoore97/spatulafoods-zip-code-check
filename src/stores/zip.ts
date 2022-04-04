f18ffea5c9f04b38ab71bdf5a190627544443a38
jakemmoore97 <36558108+jakemmoore97@users.noreply.github.com>
2022-04-04 14:59:08 -0400

    revert

--- src/stores/zip.ts
+++ src/stores/zip.ts
@@ -1,4 +1,4 @@
 import {writable} from 'svelte/store'
-const stored = localStorage.zip.slice(0,3)
+const stored = localStorage.zip
 export const zip = writable(stored || '')
 zip.subscribe(value => (localStorage.zip = value.slice(0, 3)))
