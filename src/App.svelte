6f30cd32665c09a77a202be5e2f0faaa2a747012
jakemmoore97 <36558108+jakemmoore97@users.noreply.github.com>
2022-06-08 13:23:20 -0400

    free

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 
   const title = 'Get ready for something good!'
   const description =
-    'We only deliver to certain postal codes. Please enter your six digit postal code to check current availability. All orders enjoy FREE delivery.'
+    'We only deliver to certain postal codes. Please enter your six digit postal code to check current availability.'
   const src = Image
   const alt = 'Spatula Foods'
 </script>
